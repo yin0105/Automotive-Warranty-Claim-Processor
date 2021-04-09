7ad7f15bc6e97762f65719773d1cf6b7b0b70e92
python-dev-pph <peoplepph004@gmail.com>
2021-04-09 14:56:08 +0300

    Add accounts/

---
+++ claim/accounts/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+
+from . import serializers
+
+CustomUser = get_user_model()
+
+
+class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = serializers.CustomUserRetrieveSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+    def get_object(self):
+        return self.request.user
